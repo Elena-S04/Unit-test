bb20265d3e5a3ec9d802564fc49fd4295784dcd6
Elena Spirina <espirina@list.ru>
2023-05-05 09:18:32 +0400

    corrections

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -4,8 +4,8 @@ test.each([
   ['любитель', 8, 'critical'],
   ['эксперт', 50, 'wounded'],
   ['герой', 88, 'healthy'],
-])('test', ({ _, health, expected }) => {
-  const condition = calcLiving({ health });
+])('%s test %i', ({ name, health, expected }) => {
+  const condition = calcLiving({ name, health });
   expect(condition).toBe(expected);
   expect(condition).toEqual(expected);
 });
