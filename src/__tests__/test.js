5ab03d2bf60c80ca56851524bbbf8f5d3a358239
Elena Spirina <espirina@list.ru>
2023-05-05 11:42:04 +0400

    eslint build

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,10 +1,10 @@
-import calcLiving from '../app';
+import calcLiving from "../app";
 
 test.each([
-  ['любитель', 8, 'critical'],
-  ['эксперт', 50, 'wounded'],
-  ['герой', 88, 'healthy'],
-])('%s test %i', ({ name, health, expected }) => {
+  ["любитель", 8, "critical"],
+  ["эксперт", 50, "wounded"],
+  ["герой", 88, "healthy"],
+])("%s test %i", ({ name, health, expected }) => {
   const condition = calcLiving({ name, health });
   expect(condition).toBe(expected);
   expect(condition).toEqual(expected);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const characters = [
-  { name: 'мечник', health: 10 },
-  { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
+  { name: "мечник", health: 10 },
+  { name: "маг", health: 100 },
+  { name: "лучник", health: 80 },
 ];
 const viability = characters
   .sort((a, b) => b.health - a.health)
@@ -12,12 +12,12 @@ const viability = characters
 export default function calcLiving(viability) {
   const { health } = viability;
   if (health > 51) {
-    return 'healthy';
+    return "healthy";
   }
   if (health <= 50 && health >= 15) {
-    return 'wounded';
+    return "wounded";
   }
   if (health < 15 && health >= 0) {
-    return 'critical';
+    return "critical";
   }
 }
